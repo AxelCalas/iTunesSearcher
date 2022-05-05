d8b4123b5ff79b4beabc37114e4c592d014a01eb
Axel Calas <axelcalas67@gmail.com>
2022-05-05 12:30:31 -0300

    Create basic project structure

---
+++ src/main/java/com/adtomic/iTunesSearcher/ITunesSearcherApplication.java
@@ -0,0 +1,13 @@
+package com.adtomic.iTunesSearcher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ITunesSearcherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ITunesSearcherApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/adtomic/iTunesSearcher/ITunesSearcherApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adtomic.iTunesSearcher;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ITunesSearcherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
